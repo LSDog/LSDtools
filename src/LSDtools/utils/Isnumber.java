7669cf7390e3fa12e61067ac1d1b8ec78a3595d8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-15 21:31:46 +0800

    little update

--- src/LSDtools/utils/Isnumber.java
+++ src/LSDtools/utils/Isnumber.java
@@ -3,7 +3,7 @@ package LSDtools.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Isnumber {
+public class Isnumber { //是不是正整数
 
     boolean isnum = false;
     Pattern Pnum = Pattern.compile("[0-9]*");
